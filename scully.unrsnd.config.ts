07731b22d6addafe61d2ac5c84fe3bcf775f1f83
Vladyslav Luchkov <vladyslavluchkov@cloud.upwork.com>
2023-02-07 12:28:49 +0100

    fix base href

--- scully.unrsnd.config.ts
+++ scully.unrsnd.config.ts
@@ -1,11 +1,15 @@
-import { ScullyConfig } from '@scullyio/scully';
+import { ScullyConfig, setPluginConfig } from '@scullyio/scully';
 import '@scullyio/scully-plugin-puppeteer';
+import { baseHrefRewrite } from '@scullyio/scully-plugin-base-href-rewrite';
+
+const defaultPostRenderers = ['seoHrefOptimise', baseHrefRewrite];
+setPluginConfig(baseHrefRewrite, { href: '/unrsnd/' });
 
 export const config: ScullyConfig = {
   projectRoot: './src',
   projectName: 'unrsnd',
   distFolder: './dist/unrsnd', // output directory of your Angular build artifacts
   outDir: './dist/static', // directory for scully build artifacts
-  defaultPostRenderers: [],
+  defaultPostRenderers,
   routes: {},
 };
