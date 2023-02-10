5ad45e3c6c482e62ee80a652075ce5ac72d8ef8f
Vladyslav Luchkov <vladyslavluchkov@cloud.upwork.com>
2023-02-10 20:33:19 +0100

    feat: update footer

--- src/app/footer.component.ts
+++ src/app/footer.component.ts
@@ -9,16 +9,28 @@ import { Component } from '@angular/core';
       flex
       items-center
       w-full
-      h-[10rem]
-      px-md
+      px-4xl
+      pb-[6rem]
+      pt-4xl
       bg-bg-1
       justify-between
       border-bg-body
       border-t-2
-      transition-all
     "
     >
-      Test
+      <h2 class="block h6">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br>
+        <span class="text-f-1 text">© 2023 Copyright Unrasend. All rights reserved.</span>
+      </h2>
+      <div>
+        <h2 class="h6">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+        <ul class="flex">
+          <li class="mr-xs"><a href="#">Link</a></li>
+          <li class="mr-xs"><a href="#">Link</a></li>
+          <li class="mr-xs"><a href="#">Link</a></li>
+          <li class="mr-xs"><a href="#">Link</a></li>
+        </ul>
+      </div>
     </footer>
   `,
   styles: [
